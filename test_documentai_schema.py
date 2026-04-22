ebb43602a257f5b86b9a36b59238377219564252
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2026-04-22 09:25:26 -0400

    updates

--- test_documentai_schema.py
+++
@@ -1,9 +0,0 @@
-import sys
-try:
-    from google.cloud import documentai
-    page = documentai.Document.Page()
-    print("Does page have image_block?", hasattr(page, "image_block"))
-    print("Does page have image_blocks?", hasattr(page, "image_blocks"))
-    print("Does page have visual_elements?", hasattr(page, "visual_elements"))
-except ImportError:
-    print("documentai not installed")
